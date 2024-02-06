e4eff02f8f6ac5b816e36b3aa33aaeed5a5f39c9
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-06 14:57:28 +0530

    add 404

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -9,6 +9,9 @@ import { getContact } from "../data";
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 };
 
